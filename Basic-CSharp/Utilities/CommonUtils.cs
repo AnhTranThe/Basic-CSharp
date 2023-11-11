65a04037ba08bb82333c10e23de32a3bb7815974
Anh Tran The <trantheanh132@gmail.com>
2023-11-11 15:55:50 +0700

    [INITIAL] Add CommonUtils to reslove all coomon functions and add appsetting.json

---
+++ Basic-CSharp/Utilities/CommonUtils.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Basic_CSharp.Utilities
+{
+    public static class CommonUtils
+    {
+        public static string GetConnectString()
+        {
+            var configBuilder = new ConfigurationBuilder()
+                       .SetBasePath(Directory.GetCurrentDirectory())      // file config ở thư mục hiện tại
+                       .AddJsonFile("appsetting.json");                    // nạp config định dạng JSON
+            var configurationroot = configBuilder.Build();
+
+            return configurationroot["ConnectionStrings:Default"];
+
+        }
+
+    }
+
+
+}
