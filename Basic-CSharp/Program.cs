5c29e1d3614b1c72779f8ba0d64b052c5d2f2c4b
Anh Tran The <trantheanh132@gmail.com>
2023-11-11 15:50:22 +0700

    FIX: FIX Basic-CSharp sln

---
+++ Basic-CSharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using Basic_CSharp.Controllers;
+
+namespace Basic_CSharp
+{
+    public class Program
+    {
+
+        private static async Task Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            await HomeController.Index();
+
+        }
+
+
+
+    }
+
+
+}
